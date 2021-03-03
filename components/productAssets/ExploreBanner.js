a2c2b4db3fa74e5e1023ed0b9c93597fc837c256
Marcus <marcus.whipper20@gmail.com>
2021-03-02 21:05:34 -0600

    Taking away words with links

--- components/productAssets/ExploreBanner.js
+++ components/productAssets/ExploreBanner.js
@@ -76,12 +76,7 @@ export default class ExploreBanner extends React.Component {
                   A new shopping experience
                 </p>
                 <div className="d-flex">
-                  <Link href="/collection">
-                    <a className="d-flex py-3 align-items-center font-color-black borderbottom border-color-black">
-                      <p className="mr-3">Explore products</p>
-                      <img src="/icon/arrow-long-right.svg" />
-                    </a>
-                  </Link>
+                
                 </div>
               </div>
             </div>
