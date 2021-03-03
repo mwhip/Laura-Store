f158d4cfeafd6f3c67a5b3f8d7db2d133839f28f
Marcus <marcus.whipper20@gmail.com>
2021-03-02 22:04:03 -0600

    fixing

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -6,45 +6,6 @@ const Footer = () => (
       <div className="row">
         <div className="col-12 col-sm-6 col-md-4">
           <p className="font-family-secondary font-size-display1 mb-4">
-            Commerce.js
-          </p>
-          <div className="d-flex font-color-medium mb-5 pb-3 pb-md-0 mb-md-0">
-            <div className="pr-5">
-              <a
-                href="https://commercejs.com/docs/"
-                className="mb-3 d-block font-color-medium"
-                target="_blank"
-              >
-                Documentation
-              </a>
-              <a
-                href="https://commercejs.com/features"
-                className="d-block font-color-medium"
-                target="_blank"
-              >
-                Features
-              </a>
-            </div>
-            <div>
-              <a
-                href="https://commercejs.com/company/about"
-                className="mb-3 d-block font-color-medium"
-                target="_blank"
-              >
-                About
-              </a>
-              <a
-                href="http://slack.commercejs.com/"
-                className="d-block font-color-medium"
-                target="_blank"
-              >
-                Community
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-12 col-sm-6 col-md-4">
-          <p className="font-family-secondary font-size-display1 mb-4">
             Follow us
           </p>
           <div className="d-flex font-color-medium mb-5 pb-3 pb-md-0 mb-md-0">
@@ -61,12 +22,12 @@ const Footer = () => (
         </div>
         <div className="col-12 col-md-4">
           <p className="font-family-secondary font-size-display1 mb-3">
-            Newsletter
+            Contact Information:
           </p>
           <div className="position-relative">
             <input
               className="borderbottom border-color-gray400 h-48 w-100 px-3"
-              placeholder="email address"
+              placeholder="512-739-0719"
             />
             <button className="bg-transparent position-absolute right-0 top-50 translateY--50 pr-2 h-48">
               <img src="/icon/arrow-long-right.svg" className="w-24" alt="Arrow icon"/>
