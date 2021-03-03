8139a7afe052cf38e351d1372855d1df20b4e2b6
Marcus <marcus.whipper20@gmail.com>
2021-03-02 22:07:16 -0600

    fixed

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -29,9 +29,6 @@ const Footer = () => (
               className="borderbottom border-color-gray400 h-48 w-100 px-3"
               placeholder="512-739-0719"
             />
-            <button className="bg-transparent position-absolute right-0 top-50 translateY--50 pr-2 h-48">
-              <img src="/icon/arrow-long-right.svg" className="w-24" alt="Arrow icon"/>
-            </button>
           </div>
         </div>
       </div>
